fcd8584d0ec22f9cc288af02fd82ee3b2ab1437f
Pilar Mendiburu <pilimendiburu@outlook.es>
2021-10-14 19:50:23 -0300

    Creamos clases

---
+++ TP_labo2_Mendiburu_GeonasStunf/cPosicion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_labo2_Mendiburu_GeonasStunf
+{
+    class cPosicion
+    {
+        Random r= new Random();
+        int fila;
+        int columna;
+        public void EleccionAlAzar() {
+            fila=r.Next(8);
+            columna = r.Next(8);
+        }
+    }
+}
